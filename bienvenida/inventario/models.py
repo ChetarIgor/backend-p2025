5dd1232cbaf9deec61055803ce427bb26cc0cfc8
Cesar Silva Rivera <cesarsilvar2002@gmail.com>
2025-09-11 16:43:26 -0300

    feat: Clase 11-09 html django
    Aprendimos a hacer el html en django y agregar productos a inventario.

--- bienvenida/inventario/models.py
+++ bienvenida/inventario/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=1000)
-    precio = models.DecimalField(decimal_places=2)
+    descripcion = models.CharField(max_length=100)
+    precio = models.DecimalField(decimal_places=2, max_digits=9999)
     stock = models.IntegerField(default=0)
 
